64a51f43bcf8be60f91b113de6ce2c9118bfd973
wuji <shidongzhang@xyz10.com>
2023-08-15 11:21:58 +0800

    feat: debug

--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,90 @@
 use proc_macro::TokenStream;
+use quote::{TokenStreamExt, quote};
 
-#[proc_macro_derive(CustomDebug)]
+#[proc_macro_derive(CustomDebug, attributes(debug))]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let st = syn::parse_macro_input!(input as syn::DeriveInput);
 
-    unimplemented!()
+    match do_expand(&st) {
+        Ok(token_stream) => token_stream.into(),
+        Err(e) => e.to_compile_error().into(),
+    }
+}
+
+
+fn do_expand(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let ret = generate_debug_trait(st)?;
+    return  Ok(ret);
+}
+
+type StructFields = syn::punctuated::Punctuated<syn::Field, syn::Token!(,)>;
+
+fn get_fields_from_derive_input(d: &syn::DeriveInput) -> syn::Result<&StructFields> {
+    if let syn::Data::Struct(syn::DataStruct {
+        fields: syn::Fields::Named(syn::FieldsNamed{ref named, ..}),
+        ..
+    }) = d.data {
+        return Ok(named);
+    }
+
+    Err(syn::Error::new_spanned(d, "Must define a struct, not Enum".to_string()))
+}
+
+fn generate_debug_trait(st: &syn::DeriveInput) -> syn::Result<proc_macro2::TokenStream> {
+    let fields = get_fields_from_derive_input(st)?;
+    let struct_name_ident = &st.ident;
+    let struct_name_lieral = struct_name_ident.to_string();
+
+    let mut fmt_body_stream = proc_macro2::TokenStream::new();
+
+    fmt_body_stream.extend(quote!(
+        fmt.debug_struct(#struct_name_lieral)
+    ));
+
+    for field in fields.iter() {
+        let field_name_ident = field.ident.as_ref().unwrap();
+        let field_name_lieral = field_name_ident.to_string();
+
+        let mut format_str = "{:?}".to_string();
+
+        if let Some(format) = get_custom_format_of_field(field)? {
+            format_str = format;
+        }
+
+        fmt_body_stream.extend(quote!(
+            .field(#field_name_lieral, &format_args!(#format_str, &self.#field_name_ident))
+        ))
+    }
+
+    fmt_body_stream.extend(quote!(
+        .finish()
+    ));
+
+    let ret_stream = quote!(
+        impl std::fmt::Debug for #struct_name_ident {
+            fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::fmt::Result {
+                #fmt_body_stream
+            }
+        }
+    );
+
+    Ok(ret_stream)
+}
+
+fn get_custom_format_of_field(field: &syn::Field) -> syn::Result<Option<String>> {
+    for attr in &field.attrs {
+        if let Ok(syn::Meta::NameValue(syn::MetaNameValue {
+            ref path,
+            ref lit,
+            ..
+        })) = attr.parse_meta() {
+            if path.is_ident("debug") {
+                if let syn::Lit::Str(ref ident_str) = lit {
+                    return Ok(Some(ident_str.value()));
+                }
+            }
+        }
+    }
+
+    Ok(None)
 }
